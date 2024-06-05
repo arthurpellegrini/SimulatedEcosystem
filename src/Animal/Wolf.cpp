dd77487a49eeb3147104ae753e671decdfd4b430
Arthur <arthur.pellegrini@outlook.fr>
2024-06-05 02:35:12 +0200

    Fix: Errors when merge with clement branch (#14)

--- src/Animal/Wolf.cpp
+++ src/Animal/Wolf.cpp
@@ -9,9 +9,6 @@ Wolf::Wolf(Gender gender) : Animal(gender, _maxSatiety, _lifespan) {}
 Wolf::Wolf(Gender gender, int age, int satiety) : Animal(gender, _maxSatiety, _lifespan, age, satiety) {}
 
 
-Wolf::Wolf(int age, int satiety, Gender gender) : Animal(age, satiety, gender) {}
-
-
 string Wolf::display() const {
     return "W";
 }
